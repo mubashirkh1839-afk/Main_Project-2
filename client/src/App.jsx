725e6a52a90f5d8e97b795755a4eff2865a4d713
mubashirkh1839-afk <mubashirkh1839@gmail.com>
2026-09-04 22:14:01 +0530

    Fix Home page import casing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import { Heart, ShieldCheck, Leaf } from 'lucide-react';
 
